d4e2ab01e67379817cd772f5d1a82fead86f667a
jafarsenior <jabborovjafar306@gmail.com>
2025-06-20 16:04:44 +0500

    animatsiya

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "aos/dist/aos.css";
+import AOS from "aos";
 AOS.init();
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Tilt from "react-parallax-tilt";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Tilt glareEnable={true} glareMaxOpacity={-5} scale={0.8}>
+      <App className=""/>
+    </Tilt>
   </React.StrictMode>
 );
